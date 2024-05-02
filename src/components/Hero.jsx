5e699cb7164bfd3d069a7f2a1b1ce24516d9e008
Adam Halnon <halnon_01@hotmail.com>
2024-05-02 18:57:13 +0100

    add grid item images w/ paths

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 export default function Hero() {
     return (
         <div className='hero'>
-            <img src='../images/photo-grid.png' alt='grid of photos' className='hero--photo'/>
+            <div className='grid--container'>
+                <img src='src/images/hero-grid/grid1.jpg' alt='grid1' className='grid--item' />
+                <img src='src/images/hero-grid/grid2.jpg' alt='grid2' className='grid--item' />
+                <img src='src/images/hero-grid/grid3.jpg' alt='grid3' className='grid--item' />
+                <img src='src/images/hero-grid/grid4.jpg' alt='grid4' className='grid--item' />
+                <img src='src/images/hero-grid/grid5.jpg' alt='grid5' className='grid--item' />
+                <img src='src/images/hero-grid/grid6.jpg' alt='grid6' className='grid--item' />
+                <img src='src/images/hero-grid/grid7.jpg' alt='grid7' className='grid--item' />
+                <img src='src/images/hero-grid/grid8.jpg' alt='grid8' className='grid--item' />
+                <img src='src/images/hero-grid/grid9.jpg' alt='grid9' className='grid--item' />
+            </div>
+        
             <h1>Online Experiences</h1>
             <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
         </div>
