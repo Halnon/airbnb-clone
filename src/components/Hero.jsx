ed360ff44838d50fe838a864810dbae2c3a6672d
Adam Halnon <halnon_01@hotmail.com>
2024-05-02 18:33:51 +0100

    add and export hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <div className='hero'>
+            <img src='../images/photo-grid.png' alt='grid of photos'/>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
