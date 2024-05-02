953c3c061b7ebc28e4313014a0cbf8645631a45f
Adam Halnon <halnon_01@hotmail.com>
2024-05-02 18:32:48 +0100

    import and render Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import NavBar from './components/NavBar.jsx';
+import Hero from './components/Hero.jsx';
 
 export default function App() {
     return (
         <div>
             <NavBar />
+            <Hero />
         </div>
     )
 }
