930ea6ead7e11d58320f6d2d1c26ed1a95883829
Adam Halnon <halnon_01@hotmail.com>
2024-05-02 18:06:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import NavBar from './components/NavBar.jsx';
+
+export default function App() {
+    return (
+        <NavBar />
+    )
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NavBar() {
+    return (
+        <div className='NavBar'>
+            <img src='./src/images/logo.png' alt='airbnb logo'/>
+        </div>
+    )
+}
