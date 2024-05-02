ab8ddb146f47860f702ddce368f7c7155a8f2850
Adam Halnon <halnon_01@hotmail.com>
2024-05-02 18:37:11 +0100

    add classname to img tags

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Hero() {
     return (
         <div className='hero'>
-            <img src='../images/photo-grid.png' alt='grid of photos'/>
+            <img src='../images/photo-grid.png' alt='grid of photos' className='hero--photo'/>
             <h1>Online Experiences</h1>
             <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
         </div>
