8909eee7eb5c892dbca156db89c0ae41259bf821
Adam Halnon <halnon_01@hotmail.com>
2024-05-03 10:04:45 +0100

    add kz image

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Card() {
+    return (
+        <div className='katie--zaferes'>
+            <img src='../images/katie-zaferes.png' alt='katie-zaferes'/>
+            <div className='p--first'>
+                <img src='../images/star.png'/>
+                <p>5.0 <span className='span--1'>(6) . USA</span></p>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><span className='span--2'>From $136</span> / person</p>
+        </div>
+    )
+}
