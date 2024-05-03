c0d02f915e4227c98fc755888342da37545ba2f5
Adam Halnon <halnon_01@hotmail.com>
2024-05-03 10:10:46 +0100

    import and render card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import NavBar from './components/NavBar.jsx';
 import Hero from './components/Hero.jsx';
+import Card from './components/Card.jsx';
 
 export default function App() {
     return (
         <div>
             <NavBar />
             <Hero />
+            <Card />
         </div>
     )
 }
