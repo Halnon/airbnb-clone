9607f39220385b8d41c9ffb433ad1e138054aac7
Adam Halnon <halnon_01@hotmail.com>
2024-05-03 10:09:16 +0100

    create card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,9 @@ import React from "react"
 export default function Card() {
     return (
         <div className='katie--zaferes'>
-            <img src='../images/katie-zaferes.png' alt='katie-zaferes'/>
+            <img src='src\images\katie-zaferes.png' alt='katie-zaferes'/>
             <div className='p--first'>
-                <img src='../images/star.png'/>
+                <img src='src\images\star.png' alt='star'/>
                 <p>5.0 <span className='span--1'>(6) . USA</span></p>
             </div>
             <p>Life lessons with Katie Zaferes</p>
