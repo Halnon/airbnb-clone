d018a612144c0ac846d9ac30b8127d8c761181aa
Adam Halnon <halnon_01@hotmail.com>
2024-05-03 10:34:18 +0100

    amend alt propeties for img tags as they need to be more generic for when card components uses props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,9 @@ import React from "react"
 export default function Card() {
     return (
         <div className='katie--zaferes'>
-            <img src='src\images\katie-zaferes.png' alt='katie-zaferes'/>
+            <img src='src\images\katie-zaferes.png' alt='main card image'/>
             <div className='p--first'>
-                <img src='src\images\star.png' alt='star'/>
+                <img src='src\images\star.png' alt='star icon'/>
                 <p>5.0 <span className='grey'>(6) •  USA</span></p>
             </div>
             <h2>Life lessons with Katie Zaferes</h2>
