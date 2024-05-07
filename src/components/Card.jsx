537f6a9b3d4013e5dc439f50168799f0dfccd2e8
Adam Halnon <halnon_01@hotmail.com>
2024-05-07 11:20:37 +0100

    change country to location

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export default function Card(props) {
             <img src={`public/images/${props.img}`} alt='main card image'/>
             <div className='p--first'>
                 <img src='public/images/star.png' alt='star icon'/>
-                <p>{props.rating}<span className='grey'>({props.reviewCount}) • {props.country}</span></p>
+                <p>{props.rating}<span className='grey'>({props.reviewCount}) • {props.location}</span></p>
             </div>
             <h2>{props.title}</h2>
             <p><span className='bold'>From ${props.price}</span> / person</p>
