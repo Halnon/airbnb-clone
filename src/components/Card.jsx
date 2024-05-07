c6e671813eea52672d2828c4127ae928c8941787
Adam Halnon <halnon_01@hotmail.com>
2024-05-07 10:45:15 +0100

    add props param to function and JSX elements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props)
     return (
         <div className='katie--zaferes'>
-            <img src='src\images\katie-zaferes.png' alt='main card image'/>
+            <img src={`../src/images/${props.img}`} alt='main card image'/>
             <div className='p--first'>
                 <img src='src\images\star.png' alt='star icon'/>
-                <p>5.0 <span className='grey'>(6) •  USA</span></p>
+                <p>{props.rating}<span className='grey'>({props.reviewCount}) • {props.country}</span></p>
             </div>
-            <h2>Life lessons with Katie Zaferes</h2>
-            <p><span className='bold'>From $136</span> / person</p>
+            <h2>{props.title}</h2>
+            <p><span className='bold'>From ${props.price}</span> / person</p>
         </div>
     )
 }
