285469e311affdb62145fe3053eea331a0667615
Adam Halnon <halnon_01@hotmail.com>
2024-05-10 12:07:51 +0100

    refactor props to accept data key and add additional card badge and conditional render statement

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,26 @@
 import React from "react"
 
 export default function Card(props) {
+    console.log(props)
+    let badgeText
+    if (props.data.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.data.location === 'Online') {
+        badgeText = 'ONLINE'
+    }
+
     return (
         <div className="card">
-            <img src={`../images/${props.img}`} className="card--image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../images/${props.data.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{props.data.stats.rating}</span>
+                <span className="gray">({props.data.stats.reviewCount}) • </span>
+                <span className="gray">{props.data.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.data.title}</p>
+            <p className="card--price"><span className="bold">From ${props.data.price}</span> / person</p>
         </div>
     )
 }
