046db0f41746a6ffe4e9f4a05d9de36225f194e1
Adam Halnon <halnon_01@hotmail.com>
2024-05-10 13:32:19 +0100

    refactor props input from App to accept data array from spread syntax

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,24 +3,24 @@ import React from "react"
 export default function Card(props) {
     console.log(props)
     let badgeText
-    if (props.data.openSpots === 0) {
+    if (props.openSpots === 0) {
         badgeText = 'SOLD OUT'
-    } else if (props.data.location === 'Online') {
+    } else if (props.location === 'Online') {
         badgeText = 'ONLINE'
     }
 
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`../images/${props.data.coverImg}`} className="card--image" />
+            <img src={`../images/${props.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>{props.data.stats.rating}</span>
-                <span className="gray">({props.data.stats.reviewCount}) • </span>
-                <span className="gray">{props.data.location}</span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p className="card--title">{props.data.title}</p>
-            <p className="card--price"><span className="bold">From ${props.data.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
